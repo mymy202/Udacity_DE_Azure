960c27a59016596daba16874d1f6db26dd28684c
Thi My Hao Pham <thimyhao.pham@gmail.com>
2024-02-14 11:00:10 +0700

    Update ProjectDataToPostgres.py

--- ProjectDataToPostgres.py
+++ ProjectDataToPostgres.py
@@ -52,7 +52,15 @@ def populate_table(c, filename, tablename):
 # Create Rider table
 table = "rider"
 filename = '/Users/haopham202/Documents/Udacity/project 1 - Haoptm/data/riders.csv'
-create = "CREATE TABLE rider (rider_id INTEGER PRIMARY KEY, first VARCHAR(50), last VARCHAR(50), address VARCHAR(100), birthday DATE, account_start_date DATE, account_end_date DATE, is_member BOOLEAN);"
+create = "CREATE TABLE rider (
+    rider_id INTEGER PRIMARY KEY, 
+    first VARCHAR(50), 
+    last VARCHAR(50), 
+    address VARCHAR(100), 
+    birthday DATE, 
+    account_start_date DATE, 
+    account_end_date DATE, 
+    is_member BOOLEAN);"
 
 drop_recreate(cursor, table, create)
 populate_table(cursor, filename, table)
@@ -60,7 +68,10 @@ populate_table(cursor, filename, table)
 # Create Payment table
 table = "payment"
 filename = '/Users/haopham202/Documents/Udacity/project 1 - Haoptm/data/payments.csv'
-create = "CREATE TABLE payment (payment_id INTEGER PRIMARY KEY, date DATE, amount MONEY, rider_id INTEGER);"
+create = "CREATE TABLE payment (
+    payment_id INTEGER PRIMARY KEY, 
+    date DATE, amount MONEY, 
+    rider_id INTEGER);"
 
 drop_recreate(cursor, table, create)
 populate_table(cursor, filename, table)
@@ -68,7 +79,11 @@ populate_table(cursor, filename, table)
 # Create Station table
 table = "station"
 filename = '/Users/haopham202/Documents/Udacity/project 1 - Haoptm/data/stations.csv'
-create = "CREATE TABLE station (station_id VARCHAR(50) PRIMARY KEY, name VARCHAR(75), latitude FLOAT, longitude FLOAT);"
+create = "CREATE TABLE station (
+    station_id VARCHAR(50) PRIMARY KEY, 
+    name VARCHAR(75), 
+    latitude FLOAT, 
+    longitude FLOAT);"
 
 drop_recreate(cursor, table, create)
 populate_table(cursor, filename, table)
@@ -76,7 +91,20 @@ populate_table(cursor, filename, table)
 # Create Trip table
 table = "trip"
 filename = '/Users/haopham202/Documents/Udacity/project 1 - Haoptm/data/trips.csv'
-create = "CREATE TABLE trip (trip_id VARCHAR(50) PRIMARY KEY, rideable_type VARCHAR(75), start_at TIMESTAMP, ended_at TIMESTAMP, start_station_id VARCHAR(50), end_station_id VARCHAR(50), rider_id INTEGER);"
+create = "CREATE TABLE trip (
+    trip_id VARCHAR(50) PRIMARY KEY, 
+    rideable_type VARCHAR(75), 
+    start_at TIMESTAMP, 
+    ended_at TIMESTAMP, 
+    start_station_id VARCHAR(50), 
+    end_station_id VARCHAR(50), 
+    rider_id INTEGER);"
+"CREATE TABLE trip (
+trip_id VARCHAR(50) PRIMARY KEY, 
+rideable_type VARCHAR(75), 
+start_at TIMESTAMP, 
+ended_at TIMESTAMP, 
+start_station_id VARCHAR(50), end_station_id VARCHAR(50), rider_id INTEGER);"
 
 drop_recreate(cursor, table, create)
 populate_table(cursor, filename, table)
