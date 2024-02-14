8472b16763fd61f52760cb138967ad099099c9f0
Thi My Hao Pham <thimyhao.pham@gmail.com>
2024-02-14 20:07:20 +0700

    Update and rename dim_time.sql to transform_dim_time.sql

--- transform_dim_time.sql
+++ transform_dim_time.sql
@@ -1,19 +1,17 @@
 --Create dim_time table
 IF OBJECT_ID('dbo.dim_time') IS NOT NULL
 BEGIN
-    DROP TABLE dbo.dim_time;
+    DROP EXTERNAL TABLE dbo.dim_time;
 END
 
-CREATE TABLE dim_time (
-	[time_id] [uniqueidentifier] NOT NULL,
+-- Create dim_time table
+CREATE EXTERNAL TABLE dim_time (
 	[date] [varchar](50)  NULL,
-    [day_of_week] int,
-    [day_of_month] int,
-    [week_of_year] int,
+    	[day_of_the_week] int,
+    	[day_of_the_month] int,
 	[quarter] int,
 	[month] int,
-	[year] int
-);
+	[year] int);
 
 -- Add constraint
 ALTER TABLE dbo.dim_time add CONSTRAINT PK_dim_time_time_id PRIMARY KEY NONCLUSTERED (time_id) NOT ENFORCED;
@@ -27,11 +25,9 @@ WHILE @StartDate <= @EndDate
     BEGIN
         INSERT INTO dbo.[dim_time]
         SELECT
-            NEWID(),
             @StartDate,
             DATEPART(WEEKDAY, @StartDate),
             DATEPART(DAY, @StartDate),
-            DATEPART(WEEK, @StartDate),
             DATEPART(QUARTER, @StartDate),
             DATEPART(MONTH, @StartDate),
             DATEPART(YEAR, @StartDate)
@@ -39,5 +35,5 @@ WHILE @StartDate <= @EndDate
         SET @StartDate = DATEADD(day, 1, @StartDate)
     END;
 
-
+-- Verify the output
 SELECT TOP 10 * FROM [dbo].[dim_time];
